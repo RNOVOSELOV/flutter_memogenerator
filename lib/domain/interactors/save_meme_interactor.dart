99c3227913a9fcd7aeefab7c2c70c9205b485746
RNOVOSELOV <novoselov_roman@mail.ru>
2022-12-10 22:50:20 +0300

    task5

--- lib/domain/interactors/save_meme_interactor.dart
+++ lib/domain/interactors/save_meme_interactor.dart
@@ -4,6 +4,7 @@ import 'package:memogenerator/data/models/meme.dart';
 import 'package:memogenerator/data/models/text_with_position.dart';
 import 'package:memogenerator/data/repositories/memes_repository.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
 
 class SaveMemeInteractor {
   static SaveMemeInteractor? _instance;
@@ -25,12 +26,60 @@ class SaveMemeInteractor {
     final docsPath = await getApplicationDocumentsDirectory();
     final memePath = "${docsPath.absolute.path}${Platform.pathSeparator}memes";
     await Directory(memePath).create(recursive: true);
+
     final imageName = imagePath.split(Platform.pathSeparator).last;
-    final newImagePath = "$memePath${Platform.pathSeparator}$imageName";
+    final newImagePath = "$memePath${Platform.pathSeparator}";
+
+    String newImageFullFileName = "$newImagePath$imageName";
+    final newImageFile = File(newImageFullFileName);
+    final fileExist = await newImageFile.exists();
+    // Открываем файл изображения, выбранный пользователем
     final tempFile = File(imagePath);
-    await tempFile.copy(newImagePath);
+
+    if (!fileExist) {
+      // Если файла с таким имененм в директории приложения нет, то создаем его
+      await tempFile.copy(newImageFullFileName);
+    } else {
+      final tempImageFileStats = await tempFile.stat();
+      final newImageFileStats = await newImageFile.stat();
+      if (tempImageFileStats.size != newImageFileStats.size) {
+        newImageFullFileName =
+            await _generateImageFileName(newImagePath, imageName);
+        await tempFile.copy(newImageFullFileName);
+      }
+    }
     final meme =
-        Meme(id: id, texts: textWithPositions, memePath: newImagePath);
+        Meme(id: id, texts: textWithPositions, memePath: newImageFullFileName);
+    print("Saved: $newImageFullFileName");
     return MemesRepository.getInstance().addToMemes(meme);
+  }
+
+  Future<String> _generateImageFileName(
+      String imagePath, String tempImageName) async {
+    // Проверяем существует ли уже файл с таким именем в директории приложения
+    final newImageFullFileName = "$imagePath$tempImageName";
+    final newImageFile = File(newImageFullFileName);
+    final fileExist = await newImageFile.exists();
+    if (!fileExist) {
+      return newImageFullFileName;
+    }
+    final splitDotList = tempImageName.split('.');
+    if (splitDotList.length <= 1) {
+      // Возвращаем случайное имя файла, на всякий случай, так как у текущего имени нет разрешения
+      return "$imagePath${const Uuid().v4()}";
+    }
+    // Удаляем разрешение из имени файла и храним его в переменной
+    final fileExtension = splitDotList.removeLast();
+    final getFileNumberList = splitDotList.join('.').split('_');
+    if (getFileNumberList.length <= 1) {
+      return "$imagePath${splitDotList.join('.')}_1.$fileExtension";
+    }
+
+    int? numberValue = int.tryParse(getFileNumberList.removeLast());
+    if (numberValue == null) {
+      return "$imagePath${const Uuid().v4()}.$fileExtension";
+    }
+    return await _generateImageFileName(
+        imagePath, "${getFileNumberList.join('_')}_${numberValue + 1}.$fileExtension");
   }
 }
